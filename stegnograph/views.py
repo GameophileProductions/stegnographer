673f55c287fb71ae2aea0de0c88e51520fa29e33
Achyut-0705 <71813671+Achyut-0705@users.noreply.github.com>
2021-08-31 00:02:26 +0530

    update ui plz

--- stegnograph/views.py
+++ stegnograph/views.py
@@ -6,7 +6,7 @@ from stegnographer.settings import  BASE_DIR, MEDIA_URL, MEDIA_ROOT
 from django.core.files import File
 from .mediator import *
 import PIL.Image
-import os
+import os, asyncio
 from PIL import ImageTk as itk
 # Create your views here.
 def home(request):
@@ -24,18 +24,30 @@ def home(request):
             if form_type == 'encode':
                 item  = Manupulation.objects.get(id=data.id)
                 path = os.path.join(MEDIA_ROOT,str(item.name),'encoded.tiff')
-                print(path)
                 encryption(
                     box_image_path=item.box_image_path.path,
                     container_image_path=item.container_image_path.path,
                     encryption_output_path=path)
+                item.encrypted_image_path = f'{item.name}/encoded.tiff'
+                item.save()
                 
-                item.encrypted_image_path.save('encoded.jpeg', File(open(path, 'rb')))
-                path =  os.path.join(MEDIA_URL,str(item.name),'encoded.jpeg')
-                print(path)
-                # item.delete()
-                return render(request,'base.html', {'media_download': item})
+                # return render(request,'base.html', {'media_download': item})
+                return render(request,'index.html', {'media_download': item})
             else:
                 pass
-    
-    return render(request, "base.html", {'encodeform': EncodeForm(), 'decodeform': DecodeForm()})
+    empty_db()
+    return render(request, "index.html", {'encodeform': EncodeForm(), 'decodeform': DecodeForm()})
+
+def empty_db():
+    items = Manupulation.objects.all()
+    for item in items:
+        item.encrypted_image_path.delete()
+        item.delete()
+        
+        
+                # import pdb
+                # pdb.set_trace()
+                # img_path = os.path.join(MEDIA_URL,)
+                # path =  os.path.join(MEDIA_URL,str(item.name),'encoded.jpeg')
+                # img_path = os.path.join(MEDIA_URL,str(item.name),'encoded.tiff')
+                # item.delete()
