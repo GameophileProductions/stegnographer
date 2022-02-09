7280cf649ec4403a1857be0e57a2e67c19ed707f
Achyut <achyut.shukla.btech2020@sitpune.edu.in>
2022-02-09 22:17:08 +0530

    push

--- stegnographer/settings.py
+++ stegnographer/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['gameostash.herokuapp.com', '*.stegnographer.software']
+# ALLOWED_HOSTS = ['gameostash.herokuapp.com', '*.stegnographer.software']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
