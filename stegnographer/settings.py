6ed9ab727d0d2a2dcdd2f2c2940710a7f87094a1
Achyut <achyut.shukla.btech2020@sitpune.edu.in>
2022-02-09 22:07:00 +0530

    push to production

--- stegnographer/settings.py
+++ stegnographer/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = r'*i=c&9h%-j(!-soc7q0jr=@!d5r-ch+$fw5qm*8yumdr9t=*o@'
+SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = r'*i=c&9h%-j(!-soc7q0jr=@!d5r-ch+$fw5qm*8yumdr9t=*o@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gameostash.herokuapp.com']
 
 # Application definition
 
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'stegnographer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DATABASE_NAME'),
-#         'USER': config('DATABASE_USERNAME'),
-#         'PASSWORD': config('DATABASE_PASSWORD'),
-#         'HOST': config('DATABASE_HOST')
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USERNAME'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST')
+    }
+}
 
 
 # Password validation
